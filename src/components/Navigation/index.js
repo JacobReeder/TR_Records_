9fcbbd11caa3ba289c217c0c8563eefdb417b10e
JacobReeder <jacob.reeder1@gmail.com>
2023-10-07 15:09:05 -0700

    Currently adding code for a potential contact form, may delete

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,42 +1,57 @@
 import React from 'react';
-import coverImage from "../../assets/cover/cover-image.png";
 
 
-const Nav = ({ currentPage, handlePageChange }) => {
-   
+
+function Nav(props) {
+
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+        releasesSelected,
+        setReleasesSelected
+    } = props;
+      
     
+   
     return (
-        <header style={{ backgroundImage: `url(${coverImage})`}}>
-
+        <header>
             <h2 className= "flex-row px-1">
-
-                <div>
-                    <a data-testid="link" href="/">
-                    
-                    </a>
-                </div>
+                    <a data-testid="link" href="/"></a>
             </h2>
-                <nav>
-                    <ul className="navigation">
-                        
-                        <li>
-                            <a href="#about"
-                            onClick={() => handlePageChange('About')}
-                            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
-                            About
-                            </a>
-                        </li>
-                        <li>
-                        <a href="#releases"
-                            onClick={() => handlePageChange('Releases')}
-                            className={currentPage === 'Releases' ? 'nav-link active' : 'nav-link'}>
-                            Releases
-                            </a>
-                        </li>
 
-                       
-                    </ul>
-                </nav>
+          <nav>
+              <ul className="navigation"> 
+                    <li>
+                        <a  href='#about' onClick={() => setReleasesSelected(false)}>
+                        About
+                        </a>
+                        
+                   </li>
+                   <li>
+                   <a  href='#releases' onClick={() => setReleasesSelected(false)}>
+                        Releases
+                        </a>
+                   </li>
+                   <li className={`${releasesSelected && 'navActive'}`}>
+                    <span onClick={() => setReleasesSelected(true)}>Releases</span>
+                   
+                  </li>
+                  { categories.map((category) => (
+                    <li className= { `${currentCategory.name === category.name && !releasesSelected && 'navAvtive'}`}
+                    key={category.name}>
+
+                    <span onClick={() => {
+                    setCurrentCategory(category);
+                    setReleasesSelected(false);
+                    }}>
+
+                    </span>
+                    </li>
+                    )) 
+                    }  
+              </ul>
+         </nav>
         
         </header>
        

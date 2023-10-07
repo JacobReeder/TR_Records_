eef259f4cfd3fecb9c59c2a7d52d56edb3bbfafe
JacobReeder <jacob.reeder1@gmail.com>
2023-10-07 14:52:21 -0700

    Added JSX to conditionally render pages

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
 import React, { useState } from 'react';
-import './App.css';
-
 import Navigation from './components/Navigation';
 import About from './components/About';
 import Releases from './components/Releases';
-
-
-
+import './App.css';
 
 function App() {
   
-  const [ currentPage, setCurrentPage ] = useState('About');
-
-  const renderPage = () => {
+  const [categories] = useState([
+    {
+      name: '',
+      description: '',
+    }
     
-    if (currentPage === 'About') {
-      return <About />;
-    }
-    if (currentPage === 'Releases') {
-      return <Releases />;
-    }
-     
-  
-  }
+  ]);
 
-  const handlePageChange = (page) => setCurrentPage(page);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [releasesSelected, setReleasesSelected] = useState(false);
 
   return (
-    <div>
+    
       <div> 
-        <a href='/'></a>
-        <Navigation currentPage={currentPage} handlePageChange={handlePageChange}></Navigation>
+        
+        <Navigation 
+           categories={categories}
+           setCurrentCategory={setCurrentCategory}
+           currentCategory={currentCategory}
+           releasesSelected={releasesSelected}
+           setReleasesSelected={setReleasesSelected}
+           ></Navigation>
+           <main>
+            {!releasesSelected ? (
+              <>
+            <Releases currentCategory={currentCategory}></Releases>
+            <About></About>
+            </>
+            ) : (
+              <></>
+            )}
+           </main>
 
       </div>
-      <main>
-        <div>
-          {renderPage()}
-       
-
-        </div>
-      </main>
-    </div>
+     
   );
 }
 

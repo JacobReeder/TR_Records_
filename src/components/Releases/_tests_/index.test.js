22fad040523545f14258ac456617811a324ac0b4
JacobReeder <jacob.reeder1@gmail.com>
2023-10-08 09:52:03 -0700

    Added tests to Photolist and Releases components; tests pass

---
+++ src/components/Releases/_tests_/index.test.js
@@ -0,0 +1,25 @@
+// __tests__/Releases.test.js
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Releases from '..'
+const portrait = { name: "portraits", description: "Portraits of people in my life" };
+
+afterEach(cleanup)
+
+describe('Releases component', () => {
+
+  it('renders', () => {
+    render(<Releases currentCategory={portrait} />);
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Releases currentCategory={portrait} />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+});
+
+it('renders', () => {
+  const { getByTestId } = render(<Releases currentCategory={portrait} />)
+  expect(getByTestId('h1tag')).toHaveTextContent('Portraits')
+})

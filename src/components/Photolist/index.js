50125c3a0e1ee1f0d6c5bb5f03770a63980ebd5a
JacobReeder <jacob.reeder1@gmail.com>
2023-10-08 09:16:55 -0700

    Added helpers

--- src/components/Photolist/index.js
+++ src/components/Photolist/index.js
@@ -3,43 +3,43 @@ import React, { useState } from 'react';
 
 
 const PhotoList = ({category}) => {
+
+  
+
   const [photos] = useState([
     {
-      name: '',
-      category: 'albums',
-      description: '',
+      name: 'False Figure',
+      category: 'Albums',
+      description: 'ppppp',
       
      
     },
     {
-      name: '',
-      category: 'albums',
-      description: '',
+      name: 'Torture Chamber',
+      category: 'Albums',
+      description: 'poi',
       
     },
-    {
-      name: '',
-      category: 'albums',
-      description: '',
-     
-    }
+    
 
   ]);
 
   const currentPhotos = photos.filter(photo => photo.category === category);
 
 return (
-    <div>
-      <div>
+    
+      <div className="flex-row">
       {currentPhotos.map((image, i) => (
         <img
           src={require(`../../assets/small/${category}/${i}.jpg`).default}
           alt={image.name}
+          className="img-thumbnail mx-1"
           key={image.name}
         />
       ))}
     </div>
-    </div>
+    
+    
 
 );
 };

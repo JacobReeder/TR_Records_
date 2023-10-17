6ffad81b7261b686746ace4dbba96258e4ab40bb
JacobReeder <jacob.reeder1@gmail.com>
2023-10-16 19:24:46 -0700

    Updated photolist and releases; photolist renders

--- src/components/Photolist/index.js
+++ src/components/Photolist/index.js
@@ -1,54 +1,165 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 
 
 
-const PhotoList = () => {
+function PhotoList({ category }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentPhoto, setCurrentPhoto] = useState();
 
-  const [photos] = useState([
-    {
-      name: 'False Figure',
-      img: 'https://github.com/JacobReeder/TR_Records_/blob/main/src/assets/cover/0.png',
+      const [photos] = useState([
+        {
+          name: '1 Citrus',
+          category: 'releases',
+          description: 'Release Date:',
+        },
+        {
+          name: '2 Kaspyx',
+          category: 'releases',
+          description: 'Release Date:',
+        },
+       
+       /* {
+          name: '3 Yellowcake',
+          category: 'releases',
+          description: 'Release Date:',
+        },
+        {
+          name: '4 Miserable Creature',
+          category: 'releases',
+          description: 'Release Date:',
+        },
+        {
+          name: '5 Hellish Form',
+          category: 'releases',
+          description: 'Release Date:',
+        },
+        {
+          name: '6 Embodied Torment',
+          category: 'releases',
+          description: 'Release Date:',
+        },
+        {
+          name: '7 Ghorot',
+          category: 'releases',
+          description: 'Release Date:',
+        },
+        {
+          name: '8 Mortal Embodiment - "Unbound Emptiness"',
+          category: 'releases',
+          description: 'Release Date: October 6th 2023',
+        },
+        {
+          name: '9 Without Defeat - "Absurd World"',
+          category: 'releases',
+          description: 'Release Date: October 6th, 2023',
+        },
+        {
+          name: '10 Shitbag - "Submission"',
+          category: 'releases',
+          description: 'Release Date: October 6 2023',
+        },
+        {
+          name: '11 Talk Show - "Hyperstition"',
+          category: 'releases',
+          description: 'Release Date: October 6 2023',
+        },
+        {
+          name: '12 Last - "Sifting Through the Ashes For the Belongings of Our Loved Ones"',
+          category: 'releases',
+          description: 'Release Date:',
+        },
+        {
+          name: '13 Torture Tomb - "Self Titled"',
+          category: 'releases',
+          description: 'Release Date:',
+        },
+        {
+          name: '14 Oromet - "Self Titled"',
+          category: 'releases',
+          description: 'Release Date:',
+        }, */
+        /*{
+          name: '15 Shrinkwrap Killers - "Feed the Clones"', 
+          category: 'releases',
+          description: 'Release Date:',
+        },
+        {
+          name: '',
+          category: 'releases',
+          description: 'Releases Date:',
+        },
+        {
+          name: '',
+          category: 'releases',
+          description: 'Releases Date:',
+        },
+        {
+          name: '',
+          category: 'releases',
+          description: 'Releases Date:',
+        },
+        {
+          name: '',
+          category: 'releases',
+          description: 'Releases Date:',
+        },
+        {
+          name: '',
+          category: 'releases',
+          description: 'Releases Date:',
+        },
+        {
+          name: '',
+        category: 'releases',
+        description: 'Releases Date:',
+        },
+        {
+          name: '',
+        category: 'releases',
+        description: 'Releases Date:',
+        },
+        {
+          name: '',
+        category: 'releases',
+        description: 'Releases Date:',
+        },
+        {
+          name: '',
+        category: 'releases',
+        description: 'Releases Date:',
+        },
+        */
+      ]);
       
-      
-     
-    },
-    {
-      name: 'Torture Chamber',
-      img: 'https://github.com/JacobReeder/TR_Records_/blob/main/src/assets/cover/1.png',
-     
-      
-    },
-    
+      const currentPhotos = photos.filter((photo) => photo.category === category);
 
-  ]);
-
-
-  return (
-    
-    <>
-    {photos.map((projectMapped => {
-        return (
-         <div>
-           <h2>{projectMapped.name}</h2>
+      const toggleModal = (image, i) => {
+        setCurrentPhoto({...image, index: i});
+        setIsModalOpen(!isModalOpen);
+        
+      }
       
-            <a>
-            <img
-            src={projectMapped.img}
-            alt={projectMapped.name}
-            key={projectMapped.name}
-            />
-            </a>
-      
-      
-      
-          </div>
-        )
-      } ))
-    }
-    </>
-  );
-};
+    return (
+      <div>
+        {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+        )}       
+          <div className="flex-row">
+        {currentPhotos.map((image, i) => ( 
+          <img
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            alt={image.name}
+            className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
+            key={image.name}
+          />
+        ))}
+      </div>
+      </div>
+    );
+  };
 
+  export default PhotoList;
 
-export default PhotoList;  
--- src/components/Releases/index.js
+++ src/components/Releases/index.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import PhotoList from './../Photolist'; 
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 
 
-
-
-const Releases = () => {
-
+function Releases( props ) {
+    const { currentCategory } = props;
       return (
-
-            <div>
-                  <h1>
-                        <div><PhotoList /></div>
-
-                  </h1>
-                  <h3>
-                  
-                  </h3>
-                  
-            </div>
-      )
+        <section>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
+    </section>
+  );
 }
+
+
 
 export default Releases;
 

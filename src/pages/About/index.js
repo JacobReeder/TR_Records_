c78107974bcf5932db859823817140728c59891f
JacobReeder <jacob.reeder1@gmail.com>
2023-10-16 19:22:35 -0700

    Added image in About component; turn this into a footer component

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import footerImage from "../../assets/cover/footer-image.jpg";
 
+//TODO: export this component into a footer
 
 function About() {
-  return (
-    <section className="my-5">
-      <h1 id="about"></h1>
-      <div className="my-2">
-          <p>
-             About section
-          </p>
-      </div>
-    </section>
-  );
-}
-
-export default About; 
+    return (
+      <section className="my-5">
+       <h1 id="about"></h1>
+       <img src={footerImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+       <div className="my-2">
+         <p></p>
+         </div>   
+      </section>
+    );
+  }
+  
+  export default About;

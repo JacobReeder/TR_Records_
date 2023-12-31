cfcccf4b5d01853788932a90dbbb1f8327a0a45c
JacobReeder <jacob.reeder1@gmail.com>
2023-12-31 14:30:28 -0800

    Updated release info up to 40; still need album covers

--- src/components/Photolist/index.js
+++ src/components/Photolist/index.js
@@ -45,7 +45,7 @@ function PhotoList({ category }) {
           description: 'Release Date: 7-3-2014',
         },
         {
-          name: '8 Naught "GETALBUMNAME"',
+          name: '8 Naught "Tomhyggjublus"',
           category: 'releases',
           description: 'Release Date: 10-10-2014',
         },
@@ -55,7 +55,7 @@ function PhotoList({ category }) {
           description: 'Release Date: 12-12-2014',
         },
         {
-          name: '10 Cloak "Succumb"',
+          name: '10 ClOAK "Succumb"',
           category: 'releases',
           description: 'Release Date: 3-11-2015',
         },
@@ -101,7 +101,7 @@ function PhotoList({ category }) {
           description: 'Releases Date: 11-11-2015',
         },
         {
-          name: '19 Cease "Humanity"',
+          name: '19 Cease "Unfold"',
           category: 'releases',
           description: 'Releases Date: 3-11-2016',
         },
@@ -117,7 +117,7 @@ function PhotoList({ category }) {
         description: 'Releases Date: 6-26-2016',
         },
         {
-          name: '22 Body Void "Ruin"',
+          name: '22 Body Void "Ruins"',
         category: 'releases',
         description: 'Releases Date: 6-26-2016',
         },
@@ -138,18 +138,18 @@ function PhotoList({ category }) {
         description: 'Releases Date: 12-12-2016',
         },
         {
-          name: '26 Waning "Still Hours"',
+          name: '26 WANING "Still Hours"',
         category: 'releases',
         description: 'Releases Date: 2-17-2017',
         },
         {
-          name: '27 Dismembered Carnage "Deranged Butchery"',
+          name: '27 DISMEMBERED CARNAGE "Deranged Butchery"',
         category: 'releases',
         description: 'Releases Date: 12-21-2016',
         },
         
         {
-          name: '28 Caffa "Mental Enslavement"',
+          name: '28 CAFFA "Mental Enslavement"',
         category: 'releases',
         description: 'Releases Date: 2-22-2017 ',
         },
@@ -164,10 +164,82 @@ function PhotoList({ category }) {
         description: 'Releases Date: 4-20-2017',
         },
         {
-          name: '31 Enseplucher "No Sanctity in Death "',
+          name: '31 Ensepulcher "No Sanctity in Death "',
         category: 'releases',
         description: 'Releases Date: 4-20-2017',
         },
+        /*
+        {
+          name: '32 Battle Hag "Tongue of the Earth"',
+        category: 'releases',
+        description: 'Releases Date: 5-7-2017',
+        },
+         {
+          name: '33 NOSTRUM "Infernal Tomb"',
+        category: 'releases',
+        description: 'Releases Date: 6-6-2017',
+        },
+         {
+          name: '34 Cosmic Reef Temple "Age of the Spaceborn"',
+        category: 'releases',
+        description: 'Releases Date: 6-16-2017',
+        },
+         {
+          name: '35 DWEL "Lodestone"',
+        category: 'releases',
+        description: 'Releases Date: 6-26-2016',
+        },
+         {
+          name: '36 BLAZAR "PHASE OMEGA"',
+        category: 'releases',
+        description: 'Releases Date: 7-7-2017',
+        },
+         {
+          name: '37 Owl Coven "Cosmic Void"',
+        category: 'releases',
+        description: 'Releases Date: 7-27-2017',
+        },
+         {
+          name: '38 Demonsmoke "Morphine Moonshine"',
+        category: 'releases',
+        description: 'Releases Date: 8-1-2017',
+        },
+         {
+          name: '39 ""',
+        category: 'releases',
+        description: 'Releases Date: ',
+        },
+         {
+          name: '40 ""',
+        category: 'releases',
+        description: 'Releases Date: ',
+        },
+         {
+          name: '41 ""',
+        category: 'releases',
+        description: 'Releases Date: ',
+        },
+         {
+          name: '42 ""',
+        category: 'releases',
+        description: 'Releases Date: ',
+        },
+         {
+          name: '43 ""',
+        category: 'releases',
+        description: 'Releases Date: ',
+        },
+         {
+          name: '44 ""',
+        category: 'releases',
+        description: 'Releases Date: ',
+        },
+         {
+          name: '45 ""',
+        category: 'releases',
+        description: 'Releases Date: ',
+        },
+        */
         
       ]);
       
